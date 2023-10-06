0ebbfbc5c556b1d6ddf31f9eace3b3b6c8a5ae32
dineshjverma <131961865+dineshjverma@users.noreply.github.com>
2023-10-06 12:56:49 +0530

    Update master.sh

--- master.sh
+++ master.sh
@@ -22,5 +22,4 @@ curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --de
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
-source <(kubectl completion bash) 
-echo "source <(kubectl completion bash)" >> ~/.bashrc
+
